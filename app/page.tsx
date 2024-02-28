9ca8e032f59bb0633904e3b64ff150faf6eb15d2
Nick Golitsyn <45421041+NickGolitsyn@users.noreply.github.com>
2024-02-28 20:02:12 +0000

    booking button, what we do, lato font added

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import Image from "next/image";
 import mainBg from "@/public/mainbg.jpg";
-import { Fjalla_One } from "next/font/google";
-
+import { Fjalla_One, Lato } from "next/font/google";
+const lato = Lato({
+  subsets: ["latin"],
+  weight: ["100", "300", "400", "700", "900"],
+});
 const fjalla = Fjalla_One({ subsets: ["latin"], weight: "400" });
 
 export default function Home() {
@@ -31,12 +34,80 @@ export default function Home() {
             <br />& Filming
           </h2>
         </div>
+        <button className="absolute bottom-10 left-1/2 -translate-x-1/2 rounded-full bg-gray-300 px-8 py-3 text-lg transition-all hover:bg-white">
+          Book now
+        </button>
         <Image
           aria-hidden="true"
           className="w-full"
           src={mainBg}
           alt="backgroundimage"
         />
+      </div>
+      <div className="min-h-screen bg-gray-300 pt-10">
+        <h1 className="text-center text-4xl">What we do</h1>
+        <div className="mx-auto mt-8 grid max-w-screen-lg grid-cols-1 gap-4 px-5 sm:grid-cols-3">
+          <div className="h-fit rounded-md bg-gray-400 px-6 py-5">
+            <h2 className="text-2xl">Roof Inspection</h2>
+            <p className={`mt-2 text-sm ${lato.className}`}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sit
+              amet enim sed dolor imperdiet placerat. Cras tempor metus ac purus
+              fermentum feugiat. Nullam scelerisque dictum magna facilisis
+              varius. Class aptent taciti sociosqu ad litora torquent per
+              conubia nostra, per inceptos himenaeos. Quisque semper eleifend
+              massa vel cursus. Etiam convallis augue vitae mi viverra, vitae
+              venenatis diam consequat.
+            </p>
+            <div className="mt-6 flex justify-center">
+              <a
+                href="#"
+                className="rounded-full bg-yellow-400 px-8 py-3 transition-all hover:bg-yellow-300"
+              >
+                View
+              </a>
+            </div>
+          </div>
+          <div className="h-fit rounded-md bg-gray-400 px-8 py-5">
+            <h2 className="text-2xl">Property Photography & Filming</h2>
+            <p className={`mt-2 text-sm ${lato.className}`}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sit
+              amet enim sed dolor imperdiet placerat. Cras tempor metus ac purus
+              fermentum feugiat. Nullam scelerisque dictum magna facilisis
+              varius. Class aptent taciti sociosqu ad litora torquent per
+              conubia nostra, per inceptos himenaeos. Quisque semper eleifend
+              massa vel cursus. Etiam convallis augue vitae mi viverra, vitae
+              venenatis diam consequat.
+            </p>
+            <div className="mt-6 flex justify-center">
+              <a
+                href="#"
+                className="rounded-full bg-yellow-400 px-8 py-3 transition-all hover:bg-yellow-300"
+              >
+                View
+              </a>
+            </div>
+          </div>
+          <div className="h-fit rounded-md bg-gray-400 px-8 py-5">
+            <h2 className="text-2xl">Landscape Photography & Filming</h2>
+            <p className={`mt-2 text-sm ${lato.className}`}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sit
+              amet enim sed dolor imperdiet placerat. Cras tempor metus ac purus
+              fermentum feugiat. Nullam scelerisque dictum magna facilisis
+              varius. Class aptent taciti sociosqu ad litora torquent per
+              conubia nostra, per inceptos himenaeos. Quisque semper eleifend
+              massa vel cursus. Etiam convallis augue vitae mi viverra, vitae
+              venenatis diam consequat.
+            </p>
+            <div className="mt-6 flex justify-center">
+              <a
+                href="#"
+                className="rounded-full bg-yellow-400 px-8 py-3 transition-all hover:bg-yellow-300"
+              >
+                View
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   );
